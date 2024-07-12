028d1df6b014be44e9e28e4ad2748e8c83f3d5f9
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-12 05:35:14 -0300

    feat: ✨ add lambda handler for hello endpoint
    introduces AWS Lambda function to process `APIGatewayProxyRequest` and respond with greeting message

---
+++ cmd/hello/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	name := request.PathParameters["name"]
+	message := fmt.Sprintf(" { \"Message\" : \"Hello %s \" } ", name)
+	return events.APIGatewayProxyResponse{Body: message, StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
