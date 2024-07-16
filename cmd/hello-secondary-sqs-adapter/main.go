5aff66c045adbc39d662722646da663c21bc81ec
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-16 10:27:57 -0300

    refactor: :hammer: unify handler logic in sqs adapters
    Replaced redundant SQS handler logic with a shared adapter structure. Improved message parsing.

--- cmd/hello-secondary-sqs-adapter/main.go
+++ cmd/hello-secondary-sqs-adapter/main.go
@@ -8,34 +8,27 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 
-	"go-sls-template/internal/handlers"
-	"go-sls-template/pkg/aws/sqs"
+	"go-sls-template/internal/hello/application"
+	"go-sls-template/internal/hello/domain"
 )
 
-func Handler(ctx context.Context, sqsEvent events.SQSEvent) error {
-	for _, message := range sqsEvent.Records {
-		if err := processMessage(message); err != nil {
-			log.Printf("Failed to process message: %v", err)
-			return err
-		}
-	}
-	return nil
+type body struct {
+	Type             string `json:"Type"`
+	MessageId        string `json:"MessageId"`
+	TopicArn         string `json:"TopicArn"`
+	Message          string `json:"Message"`
+	UnsubscribeURL   string `json:"UnsubscribeURL"`
+	SignatureVersion string `json:"SignatureVersion"`
+	Signature        string `json:"Signature"`
+	SigningCertURL   string `json:"SigningCertURL"`
 }
 
-func processMessage(message events.SQSMessage) error {
-	body := &sqs.Body{}
-	if err := json.Unmarshal([]byte(message.Body), body); err != nil {
-		return logAndReturnError("Failed to unmarshal message body", err)
-	}
+type sqsAdapter struct {
+	handler *application.HelloHandler
+}
 
-	msg := &handlers.HelloInput{}
-	if err := json.Unmarshal([]byte(body.Message), msg); err != nil {
-		return logAndReturnError("Failed to unmarshal inner message", err)
-	}
-
-	output := handlers.HelloSecondary(*msg)
-	log.Printf("Hello output: %v", output.Message)
-	return nil
+func newSqsAdapter(handler *application.HelloHandler) *sqsAdapter {
+	return &sqsAdapter{handler: handler}
 }
 
 func logAndReturnError(message string, err error) error {
@@ -43,6 +36,35 @@ func logAndReturnError(message string, err error) error {
 	return err
 }
 
+func (h *sqsAdapter) adapt(ctx context.Context, sqsEvent events.SQSEvent) error {
+	for _, record := range sqsEvent.Records {
+		body := &body{}
+		if err := json.Unmarshal([]byte(record.Body), body); err != nil {
+			return logAndReturnError("Failed to unmarshal message body", err)
+		}
+
+		input := application.DispactherInputMsg{}
+		if err := json.Unmarshal([]byte(body.Message), &input); err != nil {
+			return logAndReturnError("Failed to unmarshal message content", err)
+		}
+
+		helloHandlerMsg := application.HandlerInputMsg{
+			Name:   input.Message,
+			Suffix: "from secondary",
+		}
+
+		output := h.handler.Handle(ctx, helloHandlerMsg)
+
+		log.Printf("Output: %s", output.Message)
+	}
+
+	return nil
+}
+
 func main() {
-	lambda.Start(Handler)
+	service := domain.NewHello()
+	handler := application.NewHelloHandler(service)
+	adapter := newSqsAdapter(handler)
+
+	lambda.Start(adapter.adapt)
 }
