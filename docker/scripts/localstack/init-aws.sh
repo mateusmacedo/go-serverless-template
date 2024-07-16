707f74e6c22919e44eff6a1e100e4fe9f4661327
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-16 02:42:10 -0300

    feat: :sparkles: add account_id config to aws setup
    Include account_id configuration in AWS setup script to ensure consistent account settings.

--- docker/scripts/localstack/init-aws.sh
+++ docker/scripts/localstack/init-aws.sh
@@ -2,6 +2,7 @@
 aws configure set aws_access_key_id key --profile=default
 aws configure set aws_secret_access_key secret --profile=default
 aws configure set region us-east-1 --profile=default
+aws configure set account_id 000000000000 --profile=default
 
 echo "########### AWS Configure ###########"
 aws configure list --profile=default
