f3a61bffe47fe2142df7a638f4e266f7f2c2a40a
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-16 10:27:43 -0300

    refactor: :hammer: simplify hello-http-adapter
    Streamline initialization by removing redundant code and modularizing handler logic.

--- cmd/hello-http-adapter/main.go
+++ cmd/hello-http-adapter/main.go
@@ -2,66 +2,24 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"log"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sns"
 
-	"go-sls-template/internal/handlers"
+	"go-sls-template/internal/hello/application"
+	"go-sls-template/pkg/infrastructure"
 )
 
-var (
-	snsClient *sns.Client
-	topicArn  string
-)
-
-func init() {
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
-	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
-	}
-
-	snsClient = sns.NewFromConfig(cfg)
-
-	topicArn = os.Getenv("AWS_SNS_HELLO_TOPIC")
-	if topicArn == "" {
-		log.Fatalf("AWS_SNS_HELLO_TOPIC environment variable is not set")
-	}
+type httpAdapter struct {
+	handler *application.DispactherHandler
 }
 
-func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	name := request.PathParameters["name"]
-
-	messageToPublish, err := json.Marshal(handlers.HelloInput{Name: name})
-	if err != nil {
-		return createErrorResponse("Failed to marshal message", 500, err)
-	}
-
-	if err = publishMessage(ctx, string(messageToPublish)); err != nil {
-		return createErrorResponse("Failed to publish message", 500, err)
-	}
-
-	messageToRespond, err := json.Marshal(handlers.HelloOutput{Message: "Broadcasted hello to " + name})
-	if err != nil {
-		return createErrorResponse("Failed to marshal response", 500, err)
-	}
-
-	return events.APIGatewayProxyResponse{Body: string(messageToRespond), StatusCode: 200}, nil
-}
-
-func publishMessage(ctx context.Context, message string) error {
-	input := &sns.PublishInput{
-		Message:  aws.String(message),
-		TopicArn: aws.String(topicArn),
-	}
-
-	_, err := snsClient.Publish(ctx, input)
-	return err
+func newHttpAdapter(handler *application.DispactherHandler) *httpAdapter {
+	return &httpAdapter{handler: handler}
 }
 
 func createErrorResponse(message string, statusCode int, err error) (events.APIGatewayProxyResponse, error) {
@@ -69,6 +27,36 @@ func createErrorResponse(message string, statusCode int, err error) (events.APIG
 	return events.APIGatewayProxyResponse{Body: message, StatusCode: statusCode}, err
 }
 
+func (h *httpAdapter) adapt(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	name := request.PathParameters["name"]
+	msg := application.DispactherInputMsg{
+		Message: name,
+	}
+
+	err := h.handler.Handle(ctx, msg)
+	if err != nil {
+		return createErrorResponse("Failed to handle message", 500, err)
+	}
+
+	return events.APIGatewayProxyResponse{Body: "Message handled", StatusCode: 200}, nil
+}
+
 func main() {
-	lambda.Start(Handler)
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	snsClient := sns.NewFromConfig(cfg)
+
+	topicArn := os.Getenv("AWS_SNS_HELLO_TOPIC")
+	if topicArn == "" {
+		log.Fatalf("AWS_SNS_HELLO_TOPIC environment variable is not set")
+	}
+
+	dispatcher := infrastructure.NewSnsDispatcher[application.DispactherInputMsg](snsClient, topicArn)
+	handler := application.NewDispactherHandler(dispatcher)
+	adapter := newHttpAdapter(handler)
+
+	lambda.Start(adapter.adapt)
 }
