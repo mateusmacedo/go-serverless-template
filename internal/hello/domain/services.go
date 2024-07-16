ee51c3ba03de425cfcdc9c2c3a080b7db6a5b035
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-16 15:18:14 -0300

    refactor: :hammer: enhance error handling in say
    returning error in say improves robustness, aiding clearer communication of issues during execution.

--- internal/hello/domain/services.go
+++ internal/hello/domain/services.go
@@ -6,14 +6,14 @@ func NewHello() Hello {
 	return Hello{}
 }
 
-func (h *Hello) Say(input HelloInput) HelloOutput {
+func (h *Hello) Say(input HelloInput) (HelloOutput, error) {
 	if input.Suffix == "" {
 		return HelloOutput{
 			Message: "Hello " + input.Name,
-		}
+		}, nil
 	}
 
 	return HelloOutput{
 		Message: "Hello " + input.Name + " " + input.Suffix,
-	}
+	}, nil
 }
