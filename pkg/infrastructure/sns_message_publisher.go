6798fb9b0f9d3c569b12072efbea8bff82aee554
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-16 10:26:37 -0300

    feat: :sparkles: add sns message publisher
    introduce sns dispatcher to publish messages to an sns topic using aws sdk v2. facilitates async communication.

---
+++ pkg/infrastructure/sns_message_publisher.go
@@ -0,0 +1,38 @@
+package infrastructure
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+
+	"go-sls-template/pkg/application"
+)
+
+type SnsDispatcher[T any] struct {
+	client   *sns.Client
+	topicArn string
+}
+
+func NewSnsDispatcher[T any](client *sns.Client, topicArn string) application.MessageDispatcher[T] {
+	return &SnsDispatcher[T]{
+		client:   client,
+		topicArn: topicArn,
+	}
+}
+
+func (p *SnsDispatcher[T]) Dispatch(ctx context.Context, message T) error {
+	messageToPublish, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	input := &sns.PublishInput{
+		Message:  aws.String(string(messageToPublish)),
+		TopicArn: aws.String(p.topicArn),
+	}
+
+	_, err = p.client.Publish(ctx, input)
+	return err
+}
