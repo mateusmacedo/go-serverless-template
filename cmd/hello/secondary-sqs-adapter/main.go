a5da08963a161673f81fbc970bb28ae02e288305
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-07-30 00:20:50 -0300

    refactor: :hammer: simplify SQS adapter handlers
    Removed DynamoDB dependency and logging configuration.
    Condensed main handler to process SQS messages directly.

--- cmd/hello/secondary-sqs-adapter/main.go
+++ cmd/hello/secondary-sqs-adapter/main.go
@@ -2,35 +2,22 @@ package main
 
 import (
 	"context"
-	"os"
+	"fmt"
+	"log"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-
-	"go-sls-template/internal/hello/application"
-	"go-sls-template/internal/hello/domain"
-	"go-sls-template/internal/hello/infrastructure"
-	"go-sls-template/pkg/infrastructure/log"
 )
 
+func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
+	for _, message := range sqsEvent.Records {
+		log.Printf("Received message: %s", message.Body)
+		// Process the message
+		fmt.Println("Message processed:", message.Body)
+	}
+	return nil
+}
+
 func main() {
-	logger, _ := log.NewZapLogger()
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		logger.Error("Error loading AWS config", err)
-	}
-
-	dynamoClient := dynamodb.NewFromConfig(cfg)
-	tableName := os.Getenv("DYNAMODB_TABLE")
-	if tableName == "" {
-		logger.Error("DYNAMODB_TABLE environment variable is not set", nil)
-	}
-
-	service := domain.NewHello()
-	repository := infrastructure.NewDynamoDBRepository(dynamoClient, tableName)
-	handler := application.NewHelloHandler(service, repository)
-	adapter := infrastructure.NewSqsAdapter(handler, "from hello-secondary-sqs-adapter", logger)
-
-	lambda.Start(adapter.Adapt)
+	lambda.Start(handler)
 }
